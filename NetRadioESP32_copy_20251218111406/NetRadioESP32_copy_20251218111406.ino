f6e80e6bbdd269aa03c4f6cf1dd88af054a41ab9
medfordengineering <schristy@medford.k12.ma.us>
2025-12-18 11:15:12 -0500

    First Commit!
    Working ESP32 radio with I2S amplifier

---
+++ NetRadioESP32_copy_20251218111406/NetRadioESP32_copy_20251218111406.ino
@@ -0,0 +1,84 @@
+#include <WiFi.h>
+#include "Arduino.h"
+#include "WiFi.h"
+#include "Audio.h"
+
+#define WIFI_SSID "EngineeringSubNet"
+#define WIFI_PASS "password"
+
+#define I2S_DOUT 5
+#define I2S_BCLK 6
+#define I2S_LRC 9
+
+Audio audio;
+
+void setup() {
+  Serial.begin(115200);
+
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  // Volume (0-100)
+  audio.setVolume(10);
+  // Radio stream, e.g. Byte.fm
+  audio.connecttohost("http://www.byte.fm/stream/bytefm.m3u");
+}
+
+void loop() {
+  audio.loop();
+}
+
+// Print station info
+void audio_info(const char *info) {
+  Serial.print("info        ");
+  Serial.println(info);
+}
+void audio_id3data(const char *info) {  //id3 metadata
+  Serial.print("id3data     ");
+  Serial.println(info);
+}
+void audio_eof_mp3(const char *info) {  //end of file
+  Serial.print("eof_mp3     ");
+  Serial.println(info);
+}
+void audio_showstation(const char *info) {
+  Serial.print("station     ");
+  Serial.println(info);
+}
+void audio_showstreaminfo(const char *info) {
+  Serial.print("streaminfo  ");
+  Serial.println(info);
+}
+void audio_showstreamtitle(const char *info) {
+  Serial.print("streamtitle ");
+  Serial.println(info);
+}
+void audio_bitrate(const char *info) {
+  Serial.print("bitrate     ");
+  Serial.println(info);
+}
+void audio_commercial(const char *info) {  //duration in sec
+  Serial.print("commercial  ");
+  Serial.println(info);
+}
+void audio_icyurl(const char *info) {  //homepage
+  Serial.print("icyurl      ");
+  Serial.println(info);
+}
+void audio_lasthost(const char *info) {  //stream URL played
+  Serial.print("lasthost    ");
+  Serial.println(info);
+}
+void audio_eof_speech(const char *info) {
+  Serial.print("eof_speech  ");
+  Serial.println(info);
+}
